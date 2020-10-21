13589a32c0d426fb523f835a2208f058a171011d
Daniel <danielenrique9919@hotmail.com>
2020-10-21 13:22:03 -0600

    Iniciando proyecto Sistema de Compras TOO115 2020

---
+++ src/main/java/com/sistemacompras/too/TooApplication.java
@@ -0,0 +1,13 @@
+package com.sistemacompras.too;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooApplication.class, args);
+    }
+
+}
